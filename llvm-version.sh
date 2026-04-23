1fb67f271915ab730a6a1c7bfb8455601b8793f1
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2026-04-23 09:13:46 +0100

    added installation instructions

--- llvm-version.sh
+++ llvm-version.sh
@@ -7,14 +7,20 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ -z "$1" ] || [ "$1" != "14" ] && 
   echo "Usage: source llvm-version.sh [14|21]"
   return
 fi
+BREW_PREFIX=$(brew --prefix)
 if [ "$1" = "14" ]; then
-  LLVM_ADD="/usr/local/opt/llvm@14"
-  LLVM_REMOVE="/usr/local/opt/llvm@21"
+  LLVM_ADD="$BREW_PREFIX/opt/llvm@14"
+  LLVM_REMOVE="$BREW_PREFIX/opt/llvm@21"
 else
-  LLVM_ADD="/usr/local/opt/llvm@21"
-  LLVM_REMOVE="/usr/local/opt/llvm@14"
+  LLVM_ADD="$BREW_PREFIX/opt/llvm@21"
+  LLVM_REMOVE="$BREW_PREFIX/opt/llvm@14"
 fi
-
+# See https://github.com/cucapra/node-llvmc/tree/master For the node-llvm binding to work, If you build LLVM yourself, set LLVM_BUILD_LLVM_DYLIB=On to get the shared library
+export LLVM_BUILD_LLVM_DYLIB=On
+# Remove the other version and any duplicate of the target from LD_LIBRARY_PATH, then prepend
+LD_LIBRARY_PATH=$(echo "$LD_LIBRARY_PATH" | tr ':' '\n' | grep -vF "$LLVM_REMOVE/lib" | grep -vF "$LLVM_ADD/lib" | tr '\n' ':' | sed 's/:$//')
+# Prepend the new version's lib directory to LD_LIBRARY_PATH, ensuring it takes precedence over the old version.
+export LD_LIBRARY_PATH="$LLVM_ADD/lib:$LD_LIBRARY_PATH"
 # Remove the other version and any duplicate of the target from PATH, then prepend
 PATH=$(echo "$PATH" | tr ':' '\n' | grep -vF "$LLVM_REMOVE/bin" | grep -vF "$LLVM_ADD/bin" | tr '\n' ':' | sed 's/:$//')
 export PATH="$LLVM_ADD/bin:$PATH"
