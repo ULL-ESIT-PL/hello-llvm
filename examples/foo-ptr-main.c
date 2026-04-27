b7cc9316a5fead6f6647b5634a088d7154b625df
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2026-04-27 14:43:03 +0100

    training for the class

---
+++ examples/foo-ptr-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+
+// clang examples/foo-ptr-main.c examples/foo-ptr.ll -o tmp/foo
+// Declaración de la función externa definida en el .ll
+// llc -filetype=obj examples/foo-ptr.ll -o tmp/foo.o
+// clang -c examples/foo-ptr-main.c -o tmp/foo-main.o
+// clang tmp/foo-main.o tmp/foo.o -o tmp/foo
+extern int64_t foo(int64_t val, int64_t *myptr);
+
+int main() {
+    int64_t a = 10;
+    int64_t b = 5;
+    
+    // Call the LLVM function
+    int64_t result = foo(a, &b);
+    
+    printf("Product: %lld\n", result); // Should print 50
+    return 0;
+}
--- examples/foo-ptr.ll
+++ examples/foo-ptr.ll
@@ -1,4 +1,5 @@
 ; Check the syntax with llvm-as examples/foo-ptr.ll -o /dev/null
+; clang examples/foo-ptr-main.c examples/foo-ptr.ll -o tmp/foo
 define i64 @foo(i64 %val, ptr %myptr) {
   %temp = load i64, ptr %myptr
   %mul = mul i64 %val, %temp
